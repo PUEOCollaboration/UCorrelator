1bca3490b87510cd8c8cd0c768bade4a529b8d08
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2023-11-17 07:12:50 -0600

    update build system

--- LinkDef.h
+++ LinkDef.h
@@ -5,54 +5,48 @@
 #pragma link off all functions;
 #pragma link off all namespaces;
 
-#pragma link C++ namespace UCorrelator+;
-#pragma link C++ namespace UCorrelator::gui+;
-#pragma link C++ namespace UCorrelator::flags;
-#pragma link C++ namespace UCorrelator::peakfinder;
-#pragma link C++ namespace UCorrelator::shape;
-#pragma link C++ namespace UCorrelator::spectrum;
-#pragma link C++ namespace UCorrelator::image;
+#pragma link C++ namespace pueo+;
+#pragma link C++ namespace pueo::UCorrelator+;
+#pragma link C++ namespace pueo::UCorrelator::gui+;
+#pragma link C++ namespace pueo::UCorrelator::flags;
+#pragma link C++ namespace pueo::UCorrelator::peakfinder;
+#pragma link C++ namespace pueo::UCorrelator::shape;
+#pragma link C++ namespace pueo::UCorrelator::spectrum;
+#pragma link C++ namespace pueo::UCorrelator::image;
 
-#pragma link C++ class UCorrelator::KDE2D;
-#pragma link C++ class UCorrelator::peakfinder::FineMaximum;
-#pragma link C++ class UCorrelator::peakfinder::RoughMaximum;
-#pragma link C++ class UCorrelator::AntennaPositions;
-#pragma link C++ class UCorrelator::Baseline;
-#pragma link C++ class UCorrelator::TimeDependentAverage;
-#pragma link C++ class UCorrelator::TimeDependentAverageLoader;
-#pragma link C++ class UCorrelator::Correlator;
-#pragma link C++ class UCorrelator::ComplicatedNotchFilter;
-#pragma link C++ class UCorrelator::SineSubtractFilter;
-#pragma link C++ class UCorrelator::AdaptiveFilterAbby;
-#pragma link C++ class UCorrelator::CombinedSineSubtractFilter;
-#pragma link C++ class UCorrelator::AdaptiveMinimumPhaseFilter;
-#pragma link C++ class UCorrelator::AdaptiveBrickWallFilter; 
-#pragma link C++ class UCorrelator::AdaptiveButterworthFilter;
-#pragma link C++ class UCorrelator::BH13Filter;
-#pragma link C++ class UCorrelator::AntiBH13Filter;
-#pragma link C++ class UCorrelator::WaveformCombiner;
-#pragma link C++ class UCorrelator::Analyzer;
-#pragma link C++ class UCorrelator::AnalysisConfig;
-#pragma link C++ class UCorrelator::PointingResolution;
-#pragma link C++ class UCorrelator::ProbabilityMap;
-#pragma link C++ class UCorrelator::ProbabilityMap::Params;
-#pragma link C++ class UCorrelator::ProbabilityMap::Params::CollisionDetectionParams;
-#pragma link C++ class UCorrelator::ProbabilityMap::Params::BackwardParams;
-#pragma link C++ class UCorrelator::ProbabilityMap::Params::MCParams;
-#pragma link C++ class UCorrelator::PointingResolutionModel;
-#pragma link C++ class UCorrelator::PointingResolutionModelPlusHeadingError;
-#pragma link C++ class UCorrelator::PointingResolutionParSNRModel;
-#pragma link C++ class UCorrelator::ConstantPointingResolutionModel;
-#pragma link C++ class UCorrelator::HeadingErrorEstimator;
-#pragma link C++ class UCorrelator::EASFitter;
-#pragma link C++ class UCorrelator::EASFitResult;
-
-
-#pragma link C++ class UCorrelator::gui::Map+; 
-#pragma link C++ class UCorrelator::gui::SummaryText; 
-
-#pragma link C++ class AnitaNoiseMachine+;
-#pragma link C++ class PolarityMachine+;
+#pragma link C++ class pueo::UCorrelator::KDE2D;
+#pragma link C++ class pueo::UCorrelator::peakfinder::FineMaximum;
+#pragma link C++ class pueo::UCorrelator::peakfinder::RoughMaximum;
+#pragma link C++ class pueo::UCorrelator::AntennaPositions;
+#pragma link C++ class pueo::UCorrelator::Baseline;
+#pragma link C++ class pueo::UCorrelator::TimeDependentAverage;
+#pragma link C++ class pueo::UCorrelator::TimeDependentAverageLoader;
+#pragma link C++ class pueo::UCorrelator::Correlator;
+#pragma link C++ class pueo::UCorrelator::ComplicatedNotchFilter;
+#pragma link C++ class pueo::UCorrelator::SineSubtractFilter;
+#pragma link C++ class pueo::UCorrelator::AdaptiveFilterAbby;
+#pragma link C++ class pueo::UCorrelator::CombinedSineSubtractFilter;
+#pragma link C++ class pueo::UCorrelator::AdaptiveMinimumPhaseFilter;
+#pragma link C++ class pueo::UCorrelator::AdaptiveBrickWallFilter; 
+#pragma link C++ class pueo::UCorrelator::AdaptiveButterworthFilter;
+#pragma link C++ class pueo::UCorrelator::WaveformCombiner;
+#pragma link C++ class pueo::UCorrelator::Analyzer;
+#pragma link C++ class pueo::UCorrelator::AnalysisConfig;
+#pragma link C++ class pueo::UCorrelator::PointingResolution;
+#pragma link C++ class pueo::UCorrelator::ProbabilityMap;
+#pragma link C++ class pueo::UCorrelator::ProbabilityMap::Params;
+#pragma link C++ class pueo::UCorrelator::ProbabilityMap::Params::CollisionDetectionParams;
+#pragma link C++ class pueo::UCorrelator::ProbabilityMap::Params::BackwardParams;
+#pragma link C++ class pueo::UCorrelator::ProbabilityMap::Params::MCParams;
+#pragma link C++ class pueo::UCorrelator::PointingResolutionModel;
+#pragma link C++ class pueo::UCorrelator::PointingResolutionModelPlusHeadingError;
+#pragma link C++ class pueo::UCorrelator::PointingResolutionParSNRModel;
+#pragma link C++ class pueo::UCorrelator::ConstantPointingResolutionModel;
+#pragma link C++ class pueo::UCorrelator::HeadingErrorEstimator;
+#pragma link C++ class pueo::UCorrelator::EASFitter;
+#pragma link C++ class pueo::UCorrelator::EASFitResult;
+#pragma link C++ class pueo::UCorrelator::gui::Map+; 
+#pragma link C++ class pueo::UCorrelator::gui::SummaryText; 
 
 
 
