f9d3f0e1d4f03021db9c0e336edcaf922a1450f5
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2024-01-31 11:46:32 -0600

    make it so this does... something!

--- macro/doInteractive.C
+++ macro/doInteractive.C
@@ -1,10 +1,10 @@
 #include "FFTtools.h" 
 
-UCorrelator::Analyzer *doInteractive(int run = 342, int event = 58023120, bool decimated = false, bool simulated = false )
+pueo::UCorrelator::Analyzer *doInteractive(int run = 2, int event = -1, bool decimated = false, bool simulated = true )
 {
 
   FFTtools::loadWisdom("wisdom.dat"); 
-  FilterStrategy *strategy= new FilterStrategy; 
+  pueo::FilterStrategy *strategy= new pueo::FilterStrategy; 
  
 //  TF1 fn("foo"," (x < 0.2) * exp((x-0.2)/0.01)  + (x > 0.2 && x < 1.2) * (1-0.05*x) + (x > 1.2) * exp((1.2-x)/0.02)", 0,2); 
 
@@ -28,19 +28,20 @@ UCorrelator::Analyzer *doInteractive(int run = 342, int event = 58023120, bool d
 //strategy.addOperation(butter); 
 
 
-  AnitaDataset d(run,decimated, WaveCalType::kDefault, simulated ? AnitaDataset::ANITA_MC_DATA : AnitaDataset::ANITA_ROOT_DATA, AnitaDataset::kRandomizePolarity );
+  pueo::Dataset d(run,decimated, simulated ? pueo::Dataset::PUEO_MC_DATA : pueo::Dataset::PUEO_ROOT_DATA);
 
   event > 0 ? d.getEvent(event) : d.getEntry(-event); 
 
 
 
-  UCorrelator::AnalysisConfig cfg; 
+  pueo::UCorrelator::AnalysisConfig cfg; 
   cfg.nmaxima = 2; 
-  cfg.response_option = UCorrelator::AnalysisConfig::ResponseIndividualBRotter; 
-  cfg.deconvolution_method = new AnitaResponse::AllPassDeconvolution; 
+  cfg.response_option = pueo::UCorrelator::AnalysisConfig::ResponseNone; 
+//  cfg.deconvolution_method = new AnitaResponse::AllPassDeconvolution; 
   //cfg.deconvolution_method = new AnitaResponse::AllPassDeconvolution; 
   cfg.enable_group_delay= !simulated; 
   cfg.delay_to_center = true; 
+  cfg.use_forced_trigger_rms = false; 
 //  cfg.r_time_shift_correction = !simulated; 
 //  cfg.max_peak_trigger_angle =45; 
 //  cfg.correlator_theta_lowest=90; 
@@ -49,27 +50,28 @@ UCorrelator::Analyzer *doInteractive(int run = 342, int event = 58023120, bool d
   //cfg.combine_unfiltered = false; 
  
 
-  cfg.combine_nantennas = 15; 
-  cfg.zoomed_nant = 15; 
+  cfg.combine_nantennas = 24; 
+  cfg.zoomed_nant = 24; 
   cfg.use_coherent_spectra = false; 
 
-  UCorrelator::Analyzer * analyzer = new UCorrelator::Analyzer(&cfg,true); 
+  pueo::UCorrelator::Analyzer * analyzer = new pueo::UCorrelator::Analyzer(&cfg,true); 
 
 //  strategy->addOperation(new UCorrelator::DeconvolveFilter(analyzer->getResponseManager(), cfg.deconvolution_method)); 
 //  strategy->addOperation(new ALFAFilter); 
 //  strategy->addOperation(ssf); 
 
 //  FilteredAnitaEvent* ev = new FilteredAnitaEvent(d.useful(),UCorrelator::getStrategyWithKey("adsinsub_3_10_3"), d.gps(), d.header()); 
-  UCorrelator::setAdaptiveFilterSpectrumAverageNSecs(10); 
+//  pueo::UCorrelator::setAdaptiveFilterSpectrumAverageNSecs(10); 
 
-  FilteredAnitaEvent* ev = new FilteredAnitaEvent(d.useful(),UCorrelator::getStrategyWithKey("sinsub_10_3_ad_2"), d.gps(), d.header()); 
+  pueo::FilterStrategy strat; 
+  pueo::FilteredEvent* ev = new pueo::FilteredEvent(d.useful(),&strat, d.gps(), d.header()); 
 
-  printf("auto fae = (FilteredAnitaEvent *) %p;\n",ev); 
+  printf("auto fae = (pueo::FilteredEvent *) %p;\n",ev); 
 
   //ev->plotSummary(0,0); 
 
 
-  AnitaEventSummary sum; 
+  pueo::EventSummary sum; 
   analyzer->analyze(ev,&sum,d.truth()); 
   TCanvas * chpol = new TCanvas("chpol","hpol",1800,1000); 
   TCanvas * cvpol = new TCanvas("cvpol","vpol",1800,1000); 
@@ -102,6 +104,7 @@ UCorrelator::Analyzer *doInteractive(int run = 342, int event = 58023120, bool d
   FFTtools::saveWisdom("wisdom.dat"); 
 
 
+  /*
   AnitaTemplateSummary ats; 
   AnitaTemplateMachine *atm = new AnitaTemplateMachine; 
   atm->loadTemplates(); 
@@ -112,6 +115,7 @@ UCorrelator::Analyzer *doInteractive(int run = 342, int event = 58023120, bool d
   printf("cray4[0][0]: %g\n", ats.coherent[0][0].cRay[4]); 
   printf("cray4[1][0]: %g\n", ats.coherent[1][0].cRay[4]); 
 
+  */ 
 
   return analyzer; 
 }
